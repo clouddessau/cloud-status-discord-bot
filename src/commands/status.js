c40fb8a502b6cfdf86dff7cf594adf8764b14efc
Johannes Jakob <hi@johjakob.com>
2021-08-11 22:03:11 +0200

    discord.js: Use `SlashCommandBuilder`

--- src/commands/status.js
+++ src/commands/status.js
@@ -1,10 +1,13 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
 // Get [cloud] status
 const status = require('../cloudStatus.js');
 
 // Export command '/status'
 module.exports = {
-	name: 'status',
-	description: 'Replies with the current [cloud] status',
+	data: new SlashCommandBuilder()
+		.setName('status')
+		.setDescription('Replies with the current [cloud] status'),
 	async execute(interaction) {
 		const messageStaticPart = ' [cloud] is ';
 
